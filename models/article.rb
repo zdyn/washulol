47b2b77bfd212162b82c3c7d270ebc8ee3f27d32
Nathan Jervis <mirhagk@gmail.com>
2013-11-08 23:12:41 -0500

    Fixed the time for 1 day ago
    Old value missed 30 minutes. `60*60*24 = 86400` in one day

--- models/article.rb
+++ models/article.rb
@@ -17,8 +17,8 @@ class Article < Sequel::Model
       when 60..89 then return "1 minute ago"
       when 90..3569 then return "#{(diff / 60.0).round.to_s} minutes ago"
       when 3570..5399 then return "1 hour ago"
-      when 5400..84599 then return "#{(diff / (60.0 * 60)).round.to_s} hours ago"
-      when 84600..129599 then return "1 day ago"
+      when 5400..86399 then return "#{(diff / (60.0 * 60)).round.to_s} hours ago"
+      when 86400..129599 then return "1 day ago"
       else "on #{self.created_at.strftime("%b %-d, %Y")}"
     end
   end
